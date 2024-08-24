81c2e44e83f9c43162a8f1bf8b499321453235aa
rockerspace <narendrav64@gmail.com>
2024-08-24 11:45:16 -0400

    second commit

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,21 +12,57 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const books = [
+    { id: 1, title: 'Book 1', author: 'Author 1' },
+    { id: 2, title: 'Book 2', author: 'Author 2' },
+    { id: 3, title: 'Book 3', author: 'Author 3' },
+  ];
+
+  // Use JSON.stringify to display the output neatly
+  res.json(JSON.stringify(books, null, 2));
 });
+  return res.status(300).json({message: "Yet to be implemented"});
+
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
+    const isbn = req.params.isbn;
+  const books = [
+    { id: 1, title: 'Book 1', author: 'Author 1', isbn: '1234567890' },
+    { id: 2, title: 'Book 2', author: 'Author 2', isbn: '2345678901' },
+    { id: 3, title: 'Book 3', author: 'Author 3', isbn: '3456789012' },
+  ];
+
+  const book = books.find(book => book.isbn === isbn);
+
+  if (!book) {
+    res.status(404).json({ error: 'Book not found' });
+  } else {
+    res.json(book);
+  }
+});
   return res.status(300).json({message: "Yet to be implemented"});
- });
+
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
+    const author = req.params.author;
+    const books = [
+      { id: 1, title: 'Book 1', author: 'Author 1', isbn: '1234567890' },
+      { id: 2, title: 'Book 2', author: 'Author 2', isbn: '2345678901' },
+      { id: 3, title: 'Book 3', author: 'Author 1', isbn: '3456789012' },
+    ];
+  
+    const booksByAuthor = books.filter(book => book.author.toLowerCase() === author.toLowerCase());
+  
+    if (booksByAuthor.length === 0) {
+      res.status(404).json({ error: 'No books found by this author' });
+    } else {
+      res.json(booksByAuthor);
+    }
+  });
   return res.status(300).json({message: "Yet to be implemented"});
-});
+
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
